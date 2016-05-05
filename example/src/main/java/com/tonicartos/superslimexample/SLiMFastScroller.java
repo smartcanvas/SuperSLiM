99b0d0fa23c020ff6be1f4fce29ee514cd09d04d
Rafael Dominiquini <rbaboni@ciandt.com>
2016-05-05 13:49:22 -0300

    Bug fix

--- example/src/main/java/com/tonicartos/superslimexample/SLiMFastScroller.java
+++ example/src/main/java/com/tonicartos/superslimexample/SLiMFastScroller.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.tonicartos.superslim.GridSLM;
 import com.tonicartos.superslim.LayoutManager;
 
 import xyz.danoz.recyclerviewfastscroller.AbsRecyclerViewFastScroller;
@@ -71,26 +72,53 @@ public class SLiMFastScroller extends AbsRecyclerViewFastScroller implements Rec
 
         @Override
         public float calculateScrollProgress(RecyclerView recyclerView) {
-            LayoutManager layoutManager = (LayoutManager) recyclerView.getLayoutManager();
-            int lastFullyVisiblePosition = layoutManager.findLastCompletelyVisibleItemPosition();
 
-            View visibleChild = recyclerView.getChildAt(0);
-            if (visibleChild == null) {
-                return 0;
+            View visibleChild = getViewSample(recyclerView);
+
+            if (visibleChild != null) {
+                LayoutManager layoutManager = (LayoutManager) recyclerView.getLayoutManager();
+                int lastFullyVisiblePosition = layoutManager.findLastCompletelyVisibleItemPosition();
+
+                int spanCount = getSpanCount(recyclerView, visibleChild);
+
+                RecyclerView.ViewHolder holder = recyclerView.getChildViewHolder(visibleChild);
+
+                int itemHeight = holder.itemView.getHeight();
+                int recyclerHeight = recyclerView.getHeight();
+                int itemsInWindow = (recyclerHeight / itemHeight) * spanCount;
+
+                int numItemsInList = recyclerView.getAdapter().getItemCount();
+                int numScrollableSectionsInList = numItemsInList - itemsInWindow;
+                int indexOfLastFullyVisibleItemInFirstSection = numItemsInList - numScrollableSectionsInList - 1;
+
+                int currentSection = Math.max(0, lastFullyVisiblePosition - indexOfLastFullyVisibleItemInFirstSection);
+
+                float currentPosition = (float) currentSection / numScrollableSectionsInList;
+
+                return currentPosition;
             }
 
-            RecyclerView.ViewHolder holder = recyclerView.getChildViewHolder(visibleChild);
-            int itemHeight = holder.itemView.getHeight();
-            int recyclerHeight = recyclerView.getHeight();
-            int itemsInWindow = recyclerHeight / itemHeight;
+            return 0f;
+        }
 
-            int numItemsInList = recyclerView.getAdapter().getItemCount();
-            int numScrollableSectionsInList = numItemsInList - itemsInWindow;
-            int indexOfLastFullyVisibleItemInFirstSection = numItemsInList - numScrollableSectionsInList - 1;
+        private View getViewSample(RecyclerView recyclerView) {
 
-            int currentSection = Math.max(0, lastFullyVisiblePosition - indexOfLastFullyVisibleItemInFirstSection);
+            return recyclerView.getChildAt(0);
+        }
 
-            return (float) currentSection / numScrollableSectionsInList;
+        private int getSpanCount(RecyclerView recyclerView, View viewSample) {
+
+            if (viewSample == null) {
+                viewSample = getViewSample(recyclerView);
+            }
+
+            GridSLM.LayoutParams layoutParams = GridSLM.LayoutParams.from(viewSample.getLayoutParams());
+
+            if (layoutParams.getNumColumns() != -1) {
+                return layoutParams.getNumColumns();
+            } else {
+                return layoutParams.getColumnWidth() == RecyclerView.LayoutParams.MATCH_PARENT ? 1 : recyclerView.getWidth() / layoutParams.getColumnWidth();
+            }
         }
     }
 }
